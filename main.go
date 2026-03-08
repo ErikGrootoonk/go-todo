c9521b9b5bbe0a8d9ef1ff08f9eb99647b03f16c
erikg <erik.grootoonk@gmail.com>
2026-03-08 08:32:03 +0100

    go api

--- main.go
+++ main.go
@@ -1,12 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	shortGolang := "Watch the Go crash course"
-	fullGolang := "Watch Nana's full course"
-	reward := "Bananabread"
 
-	taskItems := []string{shortGolang, fullGolang, reward}
-	fmt.Println(taskItems)
+	fmt.Println("### Welcome to our to our Todo list App ###")
+	// var shortGolang = "Watch the Go crash course"
+	// var fullGolang = "Watch Nana's full course"
+	// var reward = "Bananabread"
+
+	// var taskItems = []string{shortGolang, fullGolang, reward}
+	// printTasks(taskItems)
+	// taskItems = addTask(taskItems, "Go to the gym")
+	// printTasks(taskItems)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, welcome to our Todo list App!")
+	})
+
+	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+		taskItems := []string{"Watch the Go crash course", "Watch Nana's full course", "Bananabread"}
+		for i, task := range taskItems {
+			fmt.Fprintf(w, "%d. %s\n", i+1, task)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func printTasks(taskItems []string) {
+	fmt.Println("List of my Todos")
+
+	for i, task := range taskItems {
+		fmt.Printf("%d. %s\n", i+1, task)
+	}
+}
+
+func addTask(taskItems []string, task string) []string {
+	return append(taskItems, task)
 }
